8506f4acccdb3563a1048315c5b5bbada3f5fc65
Sulaiman Ahmad <sulaiman365@gmail.com>
2022-09-06 19:38:31 +0300

    Two-player 11% done

--- main.js
+++ main.js
@@ -4,18 +4,13 @@ let gameArray = ["","","","","","","","",""];
 
 let buttonX = document.getElementById('button-X');
 let buttonO = document.getElementById('button-O');
+let display = document.getElementById('display');
 let singlePlayer = document.getElementById('single-player');
 let twoPlayer = document.getElementById('two-player');
-
 
 buttonO.addEventListener('click',() => {
     playButton = "O";   
 })
-
-
-//twoPlayer.addEventListener('click',() => {
-//    singleOrMultiplayer = 2;
-//})
 
 const buttons = document.querySelectorAll('.box');
 const player = (buttonSelectionXO,gameGridArray) => {
@@ -30,6 +25,7 @@ const player = (buttonSelectionXO,gameGridArray) => {
     console.log(playerSelection)
     if (gameGridArray[parseInt(playerSelection)] === "")
     {
+        display.textContent = "Play in progress";
         if ((buttonSelectionXO === "X" && previousChoice === "") ||  previousChoice === "X")
             {
                 gameGridArray[parseInt(playerSelection)] = "X";
@@ -43,6 +39,110 @@ const player = (buttonSelectionXO,gameGridArray) => {
                 previousChoice = "X";
             }
     }
+    if (gameGridArray[0] === "X" && gameGridArray[1] === "X" && gameGridArray[2]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[0] === "O" && gameGridArray[1] === "O" && gameGridArray[2]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[3] === "O" && gameGridArray[4] === "O" && gameGridArray[5]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[3] === "X" && gameGridArray[4] === "X" && gameGridArray[5]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[6] === "O" && gameGridArray[7] === "O" && gameGridArray[8]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[6] === "X" && gameGridArray[7] === "X" && gameGridArray[8]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[0] === "O" && gameGridArray[3] === "O" && gameGridArray[6]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[0] === "X" && gameGridArray[3] === "X" && gameGridArray[6]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[1] === "O" && gameGridArray[4] === "O" && gameGridArray[7]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[1] === "X" && gameGridArray[4] === "X" && gameGridArray[7]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[2] === "O" && gameGridArray[5] === "O" && gameGridArray[8]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[2] === "X" && gameGridArray[5] === "X" && gameGridArray[8]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[0] === "O" && gameGridArray[4] === "O" && gameGridArray[8]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[0] === "X" && gameGridArray[4] === "X" && gameGridArray[8]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if(gameGridArray[2] === "O" && gameGridArray[4] === "O" && gameGridArray[6]  === "O")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player O Wins!";
+    }
+    else if(gameGridArray[2] === "X" && gameGridArray[4] === "X" && gameGridArray[6]  === "X")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Player X Wins!";
+    }
+    else if (gameGridArray[0] !== "" && gameGridArray[1] !== "" && gameGridArray[2] !== "" && gameGridArray[3] !== "" 
+    && gameGridArray[4] !== "" && gameGridArray[5] !== "" && gameGridArray[6] !== "" && gameGridArray[7] !== "" && gameGridArray[8] !== "" 
+    && display.textContent !== "Player O Wins!" && display.textContent !== "Player X Wins!")
+    {
+        buttonSelectionXO = "";
+        previousChoice = "";
+        display.textContent = "Draw!";
+    }
     
   });
 })
@@ -54,11 +154,13 @@ const player = (buttonSelectionXO,gameGridArray) => {
     let playerOne;
     twoPlayer.addEventListener('click',() => {
         gameArray = ["","","","","","","","",""];
+        display.textContent = "Begin!";
         buttons.forEach((grids) => {
             grids.textContent = "";
         })
         buttonX.addEventListener('click',() => {
             gameArray = ["","","","","","","","",""];
+            display.textContent = "Player 1 is X, Player 2 is O.";
             buttons.forEach((grids) => {
             grids.textContent = "";
         })
@@ -69,6 +171,7 @@ const player = (buttonSelectionXO,gameGridArray) => {
         });
         buttonO.addEventListener('click',() => {
             gameArray = ["","","","","","","","",""];
+            display.textContent = "Player 1 is O, Player 2 is X.";
             buttons.forEach((grids) => {
             grids.textContent = "";
         })
@@ -77,4 +180,12 @@ const player = (buttonSelectionXO,gameGridArray) => {
     })
     return Object.assign({},playerOne)
 }
+const reset = document.getElementById('reset-btn');
+reset.addEventListener('click',() => {
+    display.textContent = "RESET";
+    gameArray = ["","","","","","","","",""];
+    buttons.forEach((grids) => {
+        grids.textContent = "";
+    });
+})
  const game = gameBoard();
